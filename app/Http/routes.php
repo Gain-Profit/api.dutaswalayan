7b5f033b962f9b40c69f32711a7f75df22ec9859
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-12 15:51:54 +0700

    route

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return 'RERTful api for DUTA SWALAYAN';
+});
+
+//$app->get('/products', function () {
+//    return app('db')->select("SELECT * FROM products");
+//});
+
+$app->get('products','ProductController@index');
+$app->post('products','ProductController@replaceProducts');
