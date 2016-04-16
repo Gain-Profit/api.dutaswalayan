3f60d5e4f75e5b44f8257bd954f75c35db578644
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-16 19:33:56 +0700

    not use eloquent and facade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,25 +10,13 @@ class ProductController extends Controller
 {
     public function index()
     {
-// with eloquent
-//        $products  = Product::all();
-//
-//        return response()->json($products);
         return app('db')->select('SELECT * FROM products');
     }
 
     public function replaceProducts(Request $request)
     {
-// with eloquent
-//        $products = Product::create($request->all());
-//
-//        return response()->json($products);
-
         $attr = ['pid' => $request->input('pid')];
         app('db')->table('products')->updateOrInsert($attr,$request->all());
-
-// with facade
-//        DB::table('products')->updateOrInsert($attr,$request->all());
 
         return response()->json($request->all());
     }
